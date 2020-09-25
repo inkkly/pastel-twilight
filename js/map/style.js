c6dd562e43f4252352f106d88f8ef21f21b1d2bc
Lea <inkkly@gmail.com>
2020-09-25 00:16:22 -0400

    Added twilight city tile and style assets

---
+++ js/map/style.js
@@ -0,0 +1,33 @@
+ig.MapStyle.registerStyle("twilight-city", "map", {
+    sheet: "media/entity/style/twilight-city-map.png",
+    hasDoorMat: false,
+    doorGlow: {
+        x: 128,
+        y: 0,
+        xCount: 1
+    },
+    teleportField: {
+        x: 0,
+        y: 48,
+        xCount: 3,
+        zHeight: 0
+    }
+});
+ig.MapStyle.registerStyle("twilight-city-inner", "map", {
+    sheet: "media/entity/style/twilight-interior-map.png",
+    hasDoorMat: true,
+    stairDoor: {
+        x: 160,
+        y: 0
+    },
+    doorVariations: {
+        metal: {
+            x: 160,
+            y: 48
+        },
+        elevator: {
+            x: 160,
+            y: 96
+        }
+    }
+});
